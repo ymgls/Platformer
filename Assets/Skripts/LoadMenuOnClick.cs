b55d61cd241b1abc195f91818471e88977552d65
ymgls <yashin-maks@mail.ru>
2025-02-10 20:47:39 +0500

    1.5.2
    переход меню ( исправление синтаксис)

--- Assets/Skripts/LoadMenuOnClick.cs
+++ Assets/Skripts/LoadMenuOnClick.cs
@@ -5,27 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LoadMenuOnClick : MonoBehaviour
 {
-    public string menuSceneName; 
+    [SerializeField]
+    private string menuSceneName;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && IsPointerOverObject())
         {
-            SceneManager.LoadScene(menuSceneName); 
+            SceneManager.LoadScene(menuSceneName);
         }
     }
 
     private bool IsPointerOverObject()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
-        if (hit.collider != null && hit.collider.gameObject == gameObject)
-        {
-            return true;
-        }
-
-        return false;
+        return hit.collider != null && hit.collider.gameObject == gameObject;
     }
 }
