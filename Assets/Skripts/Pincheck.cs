fa8903ef467850f9930c722e8de77e2cc19f74ad
ymgls <yashin-maks@mail.ru>
2025-02-11 22:30:24 +0500

    1.7.1
    отступ

---
+++ Assets/Skripts/Pincheck.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+//public class PINCheck : MonoBehaviour
+//{
+//   public InputField pinInputField;
+//   public Button submitButton;
+//  public Text messageText;
+
+    
+//  private string correctPIN = "1234";
+
+//   public void CheckPIN()
+//    {
+//      string enteredPIN = pinInputField.text;
+
+//      if (enteredPIN == correctPIN)
+//        {
+//           messageText.text = "Доступ разрешен!";
+         
+//       else
+//        {
+//          messageText.text = "Неверный PIN-код.";
+//          pinInputField.text = "";
+//       }
+//    }
+//}
